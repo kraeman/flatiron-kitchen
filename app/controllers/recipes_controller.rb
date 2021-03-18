77eb86571b6a99bc7cbba15c417ad6f4add3a61f
Krae <kraewind@gmail.com>
2021-03-18 14:20:52 -0400

    commit tree 1ccfa6533573ed1f7cb12ff95d51584e84eab8ed

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,33 @@
+class RecipesController < ApplicationController
+    def new
+        @recipe = Recipe.new
+        @ingredients = Ingredient.all
+    end
+
+    def create
+        r = Recipe.create(recipe_params)
+        redirect_to recipe_path(r)
+    end
+
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def edit
+        @recipe = Recipe.find(params[:id])
+        @ingredients= Ingredient.all
+    end
+
+    def update
+        recipe = Recipe.find(params[:id])
+        recipe.update(recipe_params)
+        redirect_to recipe_path(recipe)
+    end
+
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, ingredient_ids: [])
+    end
+end
